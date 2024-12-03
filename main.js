b4557e263140dea6b1f586a864dfa18de4b45b92
Robbie-Scog <robertxxx1007@gmail.com>
2024-12-03 21:42:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ onload = () => {
     const poetryDiv = document.createElement('div');
     poetryDiv.classList.add('poetry');
     poetryDiv.innerHTML = `
-    <p style="margin-bottom: 80px; font-size: 16px;">Tặng bạn bài thơ, của người khác làm =))))</p>
+    <p style="margin-bottom: 80px; font-size: 16px;">A poem I found and I think it suits us...</p>
     <h2 class="poetry-heading" >Gardenia's Veil</h2>
     <p style="font-size: 20px">----- Annu2 -----</p>
     <p>The stars above in endless night,</p>
@@ -21,7 +21,7 @@ onload = () => {
     <p>A flower left for none to see.</p>
     <p>Beneath the moon’s cold, watchful eye,</p>
     <p style="margin-bottom: 80px;">Lovers hide, yet cannot lie...</p>
-    <p style="font-size: 16px;">Thui hết quà rùi đó! Qua feedback lại cho bạn p đi.</p>
+    <p style="font-size: 16px;">That´s all, babe. There is still a physical gift for you waited to be opened.</p>
     `;
 
     // Append the poetry to the body
