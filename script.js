da7f331b2c7afe6e744283b4233afd011736d695
Robbie-Scog <robertxxx1007@gmail.com>
2026-05-02 17:33:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const submitPassword = document.getElementById("submit-password");
 const errorMessage = document.getElementById("error-message");
 
 // Set the correct password (without leading zero)
-const CORRECT_PASSWORD = 12142000;
+const CORRECT_PASSWORD = 10051995;
 
 // Event Listener for Password Submission
 submitPassword.addEventListener("click", () => {
@@ -17,7 +17,7 @@ submitPassword.addEventListener("click", () => {
         window.location.href = "bcard.html";
     } else {
         // Display error message if the password is incorrect
-        errorMessage.textContent = "Try again babe. It´s really simple, your birthday ;)";
+        errorMessage.textContent = "Try again bro zzz!";
     }
 });
 
